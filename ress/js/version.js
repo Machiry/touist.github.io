a61d2ef31b615bb2fd2e122ddfa0998a671fd2bd
Maël Valais <mael.valais@gmail.com>
2017-09-06 11:26:35 +0200

    refresh the look, add a link to issues and to 'other downloads'

--- ress/js/version.js
+++ ress/js/version.js
@@ -1,7 +1,7 @@
 
 var zip_all;
 var version;
-$.get("https://api.github.com/repos/"+creator+"/"+project+"/releases/latest", function (data) {
+$.get("https://api.github.com/repos/" + creator + "/" + project + "/releases/latest", function (data) {
   zip_all = data.zipball_url;
   version = getVersion(data.zipball_url);
   document.getElementById('version').innerHTML = version;
@@ -9,47 +9,47 @@ $.get("https://api.github.com/repos/"+creator+"/"+project+"/releases/latest", fu
 });
 
 
-function getVersion(zip_all){
-	slashs = zip_all.split('/');
-	return slashs[slashs.length - 1];
+function getVersion(zip_all) {
+  slashs = zip_all.split('/');
+  return slashs[slashs.length - 1];
 }
 
 //lien github
-document.getElementById('github_link').href = "https://github.com/"+creator+"/"+project;
+document.getElementById('github_link').href = "https://github.com/" + creator + "/" + project;
 
 //lien de telchargement
-function LienTelechargement(version, zip_all){
-    var base = "https://github.com/"+creator+"/"+project+"/releases/download/"+version+"/"+project+"-"+version+"-";
-    var architecture = "x64";
-    var type = ".zip";
-	  var os   = "invalide"
-    var logo = "invalide";
-    var fin = "-"+architecture+type;
+function LienTelechargement(version, zip_all) {
+  var base = "https://github.com/" + creator + "/" + project + "/releases/download/" + version + "/" + project + "-" + version + "-";
+  var architecture = "x64";
+  var type = ".zip";
+  var os = "invalide"
+  var logo = "invalide";
+  var fin = "-" + architecture + type;
 
-	if (navigator.appVersion.indexOf("Win")!=-1){
-    os    = "windows";
-    logo  = "windows";
+  if (navigator.appVersion.indexOf("Win") != -1) {
+    os = "windows";
+    logo = "windows";
   }
-	if (navigator.appVersion.indexOf("Mac")!=-1){
-     os   ="mac";
-	   logo = "apple";
+  if (navigator.appVersion.indexOf("Mac") != -1) {
+    os = "mac";
+    logo = "apple";
   }
   //if (navigator.appVersion.indexOf("X11")!=-1)       OS  ="UNIX";
-	if (navigator.appVersion.indexOf("Linux")!=-1){
-    os    = "linux";
-    logo  = "linux";
+  if (navigator.appVersion.indexOf("Linux") != -1) {
+    os = "linux";
+    logo = "linux";
   }
-    
-    lien = base + os + fin;
-    if (navigator.appVersion.indexOf("invalide")!=-1)
-        lien = "https://api.github.com/replaceos/"+creator+"/"+project+"/zipball/"+version;
-    
-    //creation icon
-    if (logo.indexOf("invalide") == -1){
-      $("#logo_dl").append('<i class="fa fa-'+logo+' fa-3" aria-hidden="true"></i>');
-    }
 
-    $('#header_download').attr('href', lien);
-    $('#main_download').attr('href', lien);
+  lien = base + os + fin;
+  if (navigator.appVersion.indexOf("invalide") != -1)
+    lien = "https://api.github.com/replaceos/" + creator + "/" + project + "/zipball/" + version;
+
+  //creation icon
+  if (logo.indexOf("invalide") == -1) {
+    $("#logo_dl").attr('class','fa fa-' + logo + ' fa-3');
+  }
+
+  $('#header_download').attr('href', lien);
+  $('#main_download').attr('href', lien);
 
 }
